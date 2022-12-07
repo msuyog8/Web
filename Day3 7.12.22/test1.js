5a773880001a64019c8c73b8cd2000e8954534c2
Suyog Mayekar <114407875+msuyog8@users.noreply.github.com>
2022-12-07 17:00:27 +0530

    Add files via upload

---
+++ Day3 7.12.22/test1.js
@@ -0,0 +1,23 @@
+function sum(a,b)
+{
+    var num1= document.getElementById('A1').value;
+    var num2= document.getElementById('A2').value;
+
+n1 = parseInt(num1);
+n2 = parseInt(num2);
+
+var x;
+var r = n1+n2;
+
+x= 
+
+    <tr>
+
+        <td> n1</td>
+        <td> n2</td>
+        <td> r</td>
+
+    </tr>
+
+
+}
